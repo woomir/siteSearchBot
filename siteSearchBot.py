26eea400fd53ba5d5ad8360e38da238b00757cab
woomir <woomir@gmail.com>
2021-06-17 23:14:45 +0900

    update

--- siteSearchBot.py
+++ siteSearchBot.py
@@ -33,7 +33,7 @@ try:
     webdriver_options = webdriver.ChromeOptions()
     webdriver_options .add_argument('--headless')
     # webdriver_options.add_argument('--start-maximized')
-    # webdriver_options .add_argument('--start-fullscreen')
+    webdriver_options .add_argument('--start-fullscreen')
     webdriver_options.add_argument('lang=ko_KR')
     webdriver_options.add_argument(
         f'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36')
