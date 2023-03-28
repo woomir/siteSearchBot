4dbecd77496a4bb9ade5bbf0159c4f8b40cc4ad6
WooMin Jin <home@WooMinui-MacBookPro.local>
2023-03-28 21:42:15 +0900

    minor 수정

--- telegramCustomFunc.py
+++ telegramCustomFunc.py
@@ -6,7 +6,7 @@ with open("./token.txt") as f:
     token = lines[0].strip()
 
 
-async def telegramSendMessage(chatId: str, campName: str, date: str, areaName="none", term='none'):
+def telegramSendMessage(chatId: str, campName: str, date: str, areaName="none", term='none'):
     bot = telegram.Bot(token=token)
     if areaName == "none" and term == "none":
         telegramMessageText = campName + ': ' + date + ' ' + ' 예약 가능'
@@ -14,7 +14,7 @@ async def telegramSendMessage(chatId: str, campName: str, date: str, areaName="n
         telegramMessageText = campName + ': ' + date + ' ' + term + '박 예약 가능'
     elif areaName != "none" and term == "none":
         telegramMessageText = campName + ': ' + date + ' ' + areaName + ' 예약 가능'
-    await bot.sendMessage(chat_id=str(chatId), text=telegramMessageText)
+    bot.sendMessage(chat_id=str(chatId), text=telegramMessageText)
 
 
 async def telegramSimpleMessage(chatId: str, text: str):
